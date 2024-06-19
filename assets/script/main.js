6dd0839e69b7ea30cc4ef3e59905e21059200325
Yokoo.Asa <kit1238114@yahoo.co.jp>
2024-06-20 00:04:17 +0900

    first commit

---
+++ assets/script/main.js
@@ -0,0 +1,18 @@
+// ============ メイン３用 =================
+$('.l-main6__slick__items').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  arrows: false,
+  centerMode: true,
+  dots: true,
+  
+  responsive: [{
+    breakpoint: 768,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      speed: 600,
+    }
+  }]
+});
